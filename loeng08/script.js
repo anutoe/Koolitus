a6a91f3d5fb815270a08bd7c9c0e0d1c7384b062
anutoe <anutoe@gmail.com>
2022-04-13 16:54:12 +0300

    Kodune töö nr 8

---
+++ loeng08/script.js
@@ -0,0 +1,12 @@
+let komplimendiElement= document.getElementById("kompliment");
+let nimi = "Mihkel";
+nimi.length = 6;
+let muutuja = prompt("Mis on sinu nimi?");
+ 
+if (nimi="<=6"){
+    alert("Küll sul on ilus lühike nimi");
+}else {
+    alert("Küll sul on suurepärane pikk nimi");
+   
+}
+
